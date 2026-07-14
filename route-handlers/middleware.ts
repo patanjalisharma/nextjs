5c2beae5364f2e9e99e9e080fed310db42a959ae
patanjalisharma <patanjali.sharma03@gmail.com>
2026-07-14 17:14:16 +0530

    undertand middlwares/proxy

---
+++ route-handlers/middleware.ts
@@ -0,0 +1,15 @@
+import {NextResponse} from "next/server"
+
+import{NextRequest} from "next/server"
+
+export function middleware(request: NextRequest) {
+    // return NextResponse.redirect(new URL("/", request.url))
+
+    if(request.nextUrl.pathname==="/profile"){
+        return NextResponse.redirect(new URL("/hello", request.url))
+}
+}
+
+// export const config = {
+//     matcher: "/profile"
+// }
